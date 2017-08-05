b46c819eb9fb389dd71e8b3c6dc79e2bdf6fee6d
JW <craigslistmail206@mail.com>
2017-08-04 23:18:01 -0700

    file name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytestsite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "locallibrary.settings")
 
     from django.core.management import execute_from_command_line
 
